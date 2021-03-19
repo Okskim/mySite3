192ffe2f54ecb0565d4b30562a1ece473a26539c
kim oksana <oks852007@mail.ru>
2021-03-20 00:58:10 +0300

    first

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+//sliders
+
+const slides = document.querySelectorAll('.sliders__slide'),
+      prev = document.querySelector('.sliders__prev'),
+      next = document.querySelector('.sliders__next'),
+      slideWrapper = document.querySelector('.sliders__wrapper'),
+      slideField = document.querySelector ('.sliders__inner'),
+      width = window.getComputedStyle(slideWrapper).width;
+
+      let slideIndex = 1;
+      let offset = 0;
+
+      slideField.style.width = 100 * slides.length + `%`;
+      slideWrapper.style.overflow = 'hidden';
+
+
+
