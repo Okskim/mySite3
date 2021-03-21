6586aa4dd23652448fd056afecc75442984a3bc1
kim oksana <oks852007@mail.ru>
2021-03-22 01:02:53 +0300

    third

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,7 @@ const slides = document.querySelectorAll('.sliders__slide'),
       slideField = document.querySelector ('.sliders__inner'),
       width = window.getComputedStyle(slideWrapper).width;
 
-      let slideIndex = 1;
+      
       let offset = 0;
 
       slideField.style.width = 100*slides.length + '%';
@@ -17,6 +17,63 @@ const slides = document.querySelectorAll('.sliders__slide'),
       slides.forEach(slide => {
           slide.style.width = width;
       });
+
+      next.addEventListener('click', () => {
+        if (offset == +width.slice(0,width.length-2)*(slides.length-1)) {
+          offset = 0;
+        } else {
+          offset+= +width.slice(0,width.length-2);
+        }
+        slideField.style.transform = `translateX(-${offset}px)`;
+      });
+
+      prev.addEventListener('click', () => {
+        if (offset == 0) {
+          offset = +width.slice(0,width.length-2)*(slides.length-1);         
+        } else {
+          offset-= +width.slice(0,width.length-2);
+        }
+        slideField.style.transform = `translateX(-${offset}px)`;
+      });
+
+      //tabs
+
+      const tabs = document.querySelectorAll('.catalog__tab'),
+            tabsContent = document.querySelectorAll('.catalog__content'),
+            tabsParent = document.querySelector('.catalog__tabs');
+
+
+            function hideTabContent () {
+                tabsContent.forEach(item => {
+                item.classList.remove('catalog__content_active');
+              });           
+
+              tabs.forEach(item => {
+                item.classList.remove('catalog__tab_active');
+              });
+           }  
+
+            function showTabContent(i=0) {
+              tabsContent[i].classList.add('catalog__content_active');              
+              tabs[i].classList.add('catalog__tab_active');
+            }
+
+            // hideTabContent();
+            showTabContent();
+
+            tabsParent.addEventListener('click', (event) => {
+              const target = event.target;
+              if(target && target.classList.contains("catalog__tab")){
+                tabs.forEach((item,i) => {
+                  if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                  }
+                });
+              }
+
+            });
+
 
 
     });
