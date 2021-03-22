213f016f1f1fc3a459493c87c1fa8359b97a2121
kim oksana <oks852007@mail.ru>
2021-03-23 01:02:45 +0300

    four

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,14 @@ const slides = document.querySelectorAll('.sliders__slide'),
       next = document.querySelector('.sliders__next'),
       slideWrapper = document.querySelector('.sliders__wrapper'),
       slideField = document.querySelector ('.sliders__inner'),
-      width = window.getComputedStyle(slideWrapper).width;
+      width = window.getComputedStyle(slideWrapper).width,
+      slider = document.querySelector('.sliders');
 
       
       let offset = 0;
+      let slideIndex = 1;
+
+      const dots =[];
 
       slideField.style.width = 100*slides.length + '%';
       slideWrapper.style.overflow = 'hidden';
@@ -18,6 +22,53 @@ const slides = document.querySelectorAll('.sliders__slide'),
           slide.style.width = width;
       });
 
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol');
+
+    indicators.classList.add('.sliders__indicators');
+    indicators.style.cssText =`
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            z-index: 15;
+            display: flex;
+            justify-content: center;
+            margin-right: 15%;
+            margin-left: 15%;
+            list-style: none;
+
+          `;
+    slider.append(indicators);
+
+    for (let i=0; i < slides.length; i++) {
+      const dot = document.createElement('li');
+      dot.setAttribute('data-slide-to', i+1);
+      dot.style.cssText =`
+          box-sizing: content-box;
+          flex: 0 1 auto;
+          width: 30px;
+          height: 6px;
+          margin-right: 3px;
+          margin-left: 3px;
+          cursor: pointer;
+          background-color: #c70101;
+          background-clip: padding-box;
+          border-top: 10px solid transparent;
+          border-bottom: 10px solid transparent;
+          opacity: .5;
+          transition: opacity .6s ease;
+          `;
+
+          if (i == 0) {
+            dot.style.opacity = '1';
+          }
+          indicators.append(dot);
+
+          dots.push(dot);
+    }
+
       next.addEventListener('click', () => {
         if (offset == +width.slice(0,width.length-2)*(slides.length-1)) {
           offset = 0;
@@ -25,6 +76,18 @@ const slides = document.querySelectorAll('.sliders__slide'),
           offset+= +width.slice(0,width.length-2);
         }
         slideField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+          slideIndex = 1;
+        
+        } else { 
+            slideIndex++;
+        }
+        
+
+        dots.forEach(dot => dot.style.opacity = '.5');          
+        dots[slideIndex - 1].style.opacity = 1;
+        
       });
 
       prev.addEventListener('click', () => {
@@ -34,7 +97,33 @@ const slides = document.querySelectorAll('.sliders__slide'),
           offset-= +width.slice(0,width.length-2);
         }
         slideField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+          slideIndex = slides.length;
+        
+        } else { 
+            slideIndex--;
+        }
+
+        dots.forEach(dot => dot.style.opacity = '.5');          
+          dots[slideIndex - 1].style.opacity = 1;
       });
+
+      dots.forEach(dot => { 
+      dot.addEventListener('click', (e) => {
+        const slideTo = e.target.getAttribute('data-slide-to');
+
+        slideIndex = slideTo;
+        offset = +width.slice(0,width.length-2)*(slideTo - 1);
+        slideField.style.transform = `translateX(-${offset}px)`;
+
+        
+
+        dots.forEach(dot => dot.style.opacity = '.5');          
+        dots[slideIndex - 1].style.opacity = 1;
+      });
+    });
+
 
       //tabs
 
@@ -58,22 +147,25 @@ const slides = document.querySelectorAll('.sliders__slide'),
               tabs[i].classList.add('catalog__tab_active');
             }
 
-            // hideTabContent();
+            hideTabContent();
             showTabContent();
 
-            tabsParent.addEventListener('click', (event) => {
-              const target = event.target;
-              if(target && target.classList.contains("catalog__tab")){
-                tabs.forEach((item,i) => {
-                  if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
+            tabsParent.addEventListener('click', (e) => {             
+              const target = e.target;
+              console.log(target);
+              if (target && target.closest('.catalog__tab')) {
+                  console.log('2click');
+                  tabs.forEach((item, i) => {
+                      if (target == item || target.parentElement == item ) {  
+                          hideTabContent();
+                          showTabContent(i);
                   }
                 });
               }
 
             });
 
+     
 
 
     });
