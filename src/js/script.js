322f3f8c39df3297a1d2a053e6a0ca5175062e80
kim oksana <oks852007@mail.ru>
2021-04-01 00:29:23 +0300

    eleven

--- src/js/script.js
+++ src/js/script.js
@@ -332,6 +332,54 @@ const consultation = document.querySelector('#consultation'),
         },2000);
       } 
 
+      //scroll pageup
+      const pageup = document.querySelector('.pageup');
+
+      window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop > 1600) {
+          pageup.style.display = 'block';
+
+        }else {
+          pageup.style.display = 'none';
+        }
+      });
+
+       let links = document.querySelectorAll('[href^="#"]'),
+        speed = 0.3;
+    
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            let widthTop = document.documentElement.scrollTop,
+                hash = this.hash,
+                toBlock = document.querySelector(hash).getBoundingClientRect().top,
+                start = null;
+
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) {
+                    start = time;
+                }
+
+                let progress = time - start,
+                    r = (toBlock < 0 ? Math.max(widthTop - progress/speed, widthTop + toBlock) : Math.min(widthTop + progress/speed, widthTop + toBlock));
+
+                    document.documentElement.scrollTo(0, r);
+
+                if (r != widthTop + toBlock) {
+                    requestAnimationFrame(step);
+                } else {
+                    location.hash = hash;
+                }
+            }
+        });
+    });
+
+
+      
+
       
 
 });
